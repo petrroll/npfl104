8c7b08fd8d9ae6183598e9aac451fd6fb7d06857
Petr Houška <houskape@gmail.com>
2019-03-09 17:11:49 +0100

    Data loading.

--- hw/my-classifiers/dta_loader.py
+++ hw/my-classifiers/dta_loader.py
@@ -1,8 +1,72 @@
 import numpy as np
 import pandas as pd
 
-import dta_utils as dta
-
 def load_adult():
-    return (dta.load_data("./adult/adult.data", [14], skiprows=1), dta.load_data("./adult/adult.test", [14], skiprows=1))
+    # dta description
+    types = {
+        'age': 'int64',
+        'workclass': 'object', # Never-worked not in data
+        'fnlwgt': 'int64',
+        'education': 'object',
+        'education-num': 'int64',
+        'marital-status': 'object',
+        'occupation': 'object',
+        'relationship': 'object',
+        'race': 'object',
+        'sex': 'object',
+        'capital-gain': 'int64',
+        'capital-loss': 'int64',
+        'hours-per-week': 'int64',
+        'native-country': 'object',
+        'target': 'object'
+    }
 
+    names = list(types.keys())
+
+    # load data
+    train = pd.read_csv('./adult/adult.data', names=names, dtype=types, index_col=False, skipinitialspace=True)
+    test = pd.read_csv('./adult/adult.test', names=names, dtype=types, index_col=False, skipinitialspace=True, skiprows=1)
+
+    # merge data so that we have info about all potential classes & can process everything at once
+    train_size = len(train)
+    tog = train.append(test)
+
+    # change object (string) columns to category type
+    for col in tog.columns[np.where(tog.dtypes == 'object')]:
+        tog[col] = tog[col].astype('category')
+
+    # clean data
+    tog['target'] = tog['target'].str.rstrip('.')
+
+    return (tog[:train_size], tog[train_size:])
+
+def load_synth(name_base):
+    # dta description
+    types = {
+        "size": "object",
+        "color": "object",
+        "type": "object",
+        "target": "object",
+    }
+
+    names = list(types.keys())
+
+    # load data
+    train = pd.read_csv(f'./artificial_objects/{name_base}_train.csv', names=names,     dtype=types, index_col=False, skipinitialspace=True)
+    test = pd.read_csv(f'./artificial_objects/{name_base}_test.csv', names=names,   dtype=types, index_col=False, skipinitialspace=True)
+
+    # merge data so that we have info about all potential   classes & can process everything at once
+    train_size = len(train)
+    tog = train.append(test)
+
+    # change object (string) columns to category type
+    for col in tog.columns[np.where(tog.dtypes ==   'object')]:
+        tog[col] = tog[col].astype('category')
+
+    return (tog[:train_size], tog[train_size:])
+
+def load_synth_sep():
+    return load_synth('artificial_separable')
+
+def load_synth_noise():
+    return load_synth('artificial_with_noise')
