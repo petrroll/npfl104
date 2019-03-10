4cc4b42a6524100831c84cab2c280ead2ce92648
Petr Houška <houskape@gmail.com>
2019-03-10 19:31:22 +0100

    Epochs perceptron.

--- hw/my-classifiers/models/perc.py
+++ hw/my-classifiers/models/perc.py
@@ -6,23 +6,35 @@ class Perc(Model):
     '''
     Perceptron model, assumes only two target classes with values 0/1.
     '''
-    def __init__(self, lr = 0.03):
+    def __init__(self, lr = 0.03, lr_decay = 0.99, epochs = 2):
         self.lr = lr
+        self.lr_decay = lr_decay
+        self.epochs = epochs
 
     def Build(self, inputs, targets):       
-        dim = inputs.shape[1]
 
         # prepare weights + inputs array, prepend column of 1 for bias
-        w = np.random.normal(loc = 0.0, scale = 1/dim, size = dim+1)
+        dim = inputs.shape[1]
+        self.w = np.random.normal(loc = 0.0, scale = 1/2, size = dim+1)
         b = np.insert(inputs, 0, values=1, axis=1) 
+
+        for i in range(self.epochs):
+            self.__update_weigts(b, targets)
+            self.lr *= self.lr_decay
+
+
+    def __update_weigts(self, b, targets):
+        w = self.w
+        lr = self.lr
 
         # update weights for each input data
         for i in range(1, len(targets)):
             result = np.dot(b[i], w) > 0
             if result != targets[i]:
-                w = w - self.lr * (w - b[i]) * (targets[i] - result) # w -= lr * (w-b[i]) * errorPolarity
+                w = w - lr * (w - b[i]) * (targets[i] - result) # w -= lr * (w-b[i]) * errorPolarity
 
-        self.w = w
+        self.weights = w
+
 
     def Predict(self, input):
         b = np.insert(input, 0, values=1, axis=0) 
--- hw/my-classifiers/perc_runner.py
+++ hw/my-classifiers/perc_runner.py
@@ -5,10 +5,10 @@ from dta_loader import load_adult, load_synth_sep, load_synth_noise
 from validation_runner import run
 
 a_train, a_test = load_synth_noise()
-print(run(a_train, a_test, 'target', Perc()))
+print(run(a_train, a_test, 'target', Perc(0.05, 0.995, 1000)))
 
 a_train, a_test = load_synth_sep()
-print(run(a_train, a_test, 'target', Perc()))
+print(run(a_train, a_test, 'target', Perc(0.05, 0.995, 1000)))
 
 a_train, a_test = load_adult()
-print(run(a_train, a_test, 'target', Perc()))
+print(run(a_train, a_test, 'target', Perc(0.05, 0.85, 20)))
