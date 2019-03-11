0cf35bcafb927b1f0095e617d53cc0f4577abe48
Petr Houška <houskape@gmail.com>
2019-03-11 11:42:15 +0100

    Knn runner finished.

--- hw/my-classifiers/knn_runner.py
+++ hw/my-classifiers/knn_runner.py
@@ -1,9 +1,13 @@
 from models.knn import KNN
 from dta_loader import load_adult, load_synth_sep, load_synth_noise
 from validation_runner import run
+import time
 
 a_train, a_test = load_synth_noise()
 print(run(a_train, a_test, 'target', KNN(k=3)))
 
 a_train, a_test = load_synth_sep()
 print(run(a_train, a_test, 'target', KNN(k=3)))
+
+a_train, a_test = load_adult(True, 0)
+print(run(a_train, a_test, 'target', KNN(k=5)))
