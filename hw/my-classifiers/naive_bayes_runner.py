2d429db63c82310c95ef5e884e2c4be387a6e354
Petr Houška <houskape@gmail.com>
2019-03-13 00:28:02 +0100

    Add option to disable one-hot

--- hw/my-classifiers/naive_bayes_runner.py
+++ hw/my-classifiers/naive_bayes_runner.py
@@ -5,14 +5,11 @@ from models.naive_bayes import NaiveBayes
 from dta_loader import load_adult, load_synth_sep, load_synth_noise
 from validation_runner import run
 
-
-# Consider pure categorical instead of onehot for bayes 
-
 a_train, a_test = load_synth_noise()
-print(run(a_train, a_train, 'target', NaiveBayes()))
+print(run(a_train, a_train, 'target', NaiveBayes(), one_hot=False))
 
 a_train, a_test = load_synth_sep()
-print(run(a_train, a_test, 'target', NaiveBayes()))
+print(run(a_train, a_test, 'target', NaiveBayes(), one_hot=False))
 
 a_train, a_test = load_adult(False, 50)
-print(run(a_train, a_test, 'target', NaiveBayes()))
+print(run(a_train, a_test, 'target', NaiveBayes(), one_hot=False))
--- hw/my-classifiers/validation_runner.py
+++ hw/my-classifiers/validation_runner.py
@@ -1,17 +1,26 @@
 import pandas as pd
+import numpy as np
+
+# TODO: Move one_hot encoding decision to data loading
 
 # cuts pd dataframe into two numpy arrays, one with features the other with targets
-def prepare_data(data, target_col='target'):
-    features = pd.get_dummies(data.drop(target_col, axis=1)).values
-    targets = data[target_col].cat.codes
+def prepare_data(data, target_col='target', one_hot = True):
+    if one_hot:
+        features = pd.get_dummies(data.drop(target_col, axis=1)).values
+    else:
+        dta = data.drop(target_col, axis=1)
+        for col in dta.columns[np.where(dta.dtypes == 'category')]:
+            dta[col]  = dta[col].cat.codes
+        features = dta.values
 
+    targets = data[target_col].cat.codes
     return (features, targets)
 
 
-def run(train, test, target_col, model, print_acc_each=None):
+def run(train, test, target_col, model, one_hot = True, print_acc_each=None):
     # prepare data in np.array formats
-    train_i, train_t = prepare_data(train, target_col) 
-    test_i, test_t = prepare_data(test, target_col) 
+    train_i, train_t = prepare_data(train, target_col, one_hot) 
+    test_i, test_t = prepare_data(test, target_col, one_hot) 
 
     # build model
     model.Build(train_i, train_t)
